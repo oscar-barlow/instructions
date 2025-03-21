3edb73bc24fedcb581b549f41ed87c1f8e59524f
Oscar Barlow <oscar-barlow@users.noreply.github.com>
2025-03-21 14:35:56 +0000

    first commit :sunrise:

---
+++ instructions/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-v5uwkx%o(*uj2-@u#5#oy)bm=rpjh$t@1d63b50j84e_cyq)xp"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
